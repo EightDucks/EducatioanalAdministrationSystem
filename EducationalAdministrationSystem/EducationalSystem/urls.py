9d2d59629ebc276ff5548d0a9894d41174f4d4c3
GreatBeast <zhuzhn999@163.com>
2017-06-27 16:57:42 +0800

    v1.2 views

--- EducationalAdministrationSystem/EducationalSystem/urls.py
+++ EducationalAdministrationSystem/EducationalSystem/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^jiaowu/$', views.jiaowu, name='jiaowu')
 ]
