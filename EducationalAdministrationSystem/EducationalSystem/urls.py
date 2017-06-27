18e2802dd668dfa12c8a516fc7d1fb28551a7f69
magry <mgr2009@msn.com>
2017-06-27 21:52:49 +0800

    Delete *.pyc

--- EducationalAdministrationSystem/EducationalSystem/urls.py
+++ EducationalAdministrationSystem/EducationalSystem/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^jiaowu/$', views.jiaowu, name='jiaowu')
+    url(r'^jiaowu/$', views.jiaowu, name='jiaowu'),
+    # 教务、学生、教师页头部
+	url(r'^header.html$', views.header, name = 'header'),
 ]
