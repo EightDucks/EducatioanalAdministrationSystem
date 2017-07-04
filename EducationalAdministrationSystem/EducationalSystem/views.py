2c5af174a3012eb74ac4f9bddded525d0376512f
magry <mgr2009@msn.com>
2017-07-05 01:29:45 +0800

    filename

--- EducationalAdministrationSystem/EducationalSystem/views.py
+++ EducationalAdministrationSystem/EducationalSystem/views.py
@@ -1781,18 +1781,9 @@ def downloadOwnHw(request, asn_id):
     path = asn_res[0].path
     everypath = path.split('/')
     filename = everypath[-1]
-    def file_iterator(file_name, chunk_size=512):
-        with open(file_name) as f:
-            while True:
-                c = f.read(chunk_size)
-                if c:
-                    yield c
-                else:
-                    break
-    response = StreamingHttpResponse(file_iterator(path))
-    # response = HttpResponse()
+    response = HttpResponse()
     response['Content-Type'] = 'application/octet-stream'
-    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(path)
-    # content = open(path, 'rb').read()
-    # response.write(content)
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(filename)
+    content = open(path, 'rb').read()
+    response.write(content)
     return response
