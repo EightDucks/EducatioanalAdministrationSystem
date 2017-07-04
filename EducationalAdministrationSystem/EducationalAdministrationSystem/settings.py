51b6027aae1316f7436283657e8504077564badb
magry <mgr2009@msn.com>
2017-07-05 00:03:45 +0800

    debug

--- EducationalAdministrationSystem/EducationalAdministrationSystem/settings.py
+++ EducationalAdministrationSystem/EducationalAdministrationSystem/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-reload(sys)
-sys.setdefaultencoding("utf-8")
-
-PROJECT_ROOT = path.dirname(path.dirname(__file__)).decode(sys.getfilesystemencoding()).encode('utf-8')
+import importlib, sys
+importlib.reload(sys)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
